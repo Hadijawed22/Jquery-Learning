f066f7ceee5b372caf334c1ba5bc724978e5d8a0
Hadi Jawed <hadijawed132@gmail.com>
2023-07-25 02:45:57 +0500

    .

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     console.log('jquery activated');
     $("h1").css("color","red"); 
+    // $("h1").addclass("RANDOM CLASS");
+    // $("h1").removeclass("RANDOM CLASS");
+    $("h1").text("ADD SOME NEW TEXT");
+    $("img").attr("src");  get value
+    $("a").attr("href","SET VAlue");
+
+
+    fadeout
+    fadein
+    hide
+    toggle
+    slideup
+    slidedown
+    slidetoggle
+    .animate
 });
+*/
+
+
+
+$("h1").click(function(){
+    $("h1").toggle(); 
+})
